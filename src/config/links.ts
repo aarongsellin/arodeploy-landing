f473324335a7c4af0977f0b1b7b84811e15a803f
aarongsellin <aaron.gotthardsson@gmail.com>
2026-03-17 15:07:17 +0100

    good enough hero

---
+++ src/config/links.ts
@@ -0,0 +1,6 @@
+export const links = {
+  console: "https://console.arodeploy.com",
+  docs: "https://docs.arodeploy.com",
+  status: "https://status.arodeploy.com",
+  site: "https://arodeploy.com",
+} as const;
