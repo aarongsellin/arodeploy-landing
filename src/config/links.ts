dc84239239f19defb9f2a2a8dbf4eaedbc013ad2
Aaron <aaron.gotthardsson@gmail.com>
2026-04-16 11:12:18 +0200

    static landing

--- src/config/links.ts
+++
@@ -1,6 +0,0 @@
-export const links = {
-  console: "https://console.arodeploy.com",
-  docs: "https://docs.arodeploy.com",
-  status: "https://status.arodeploy.com",
-  site: "https://arodeploy.com",
-} as const;
